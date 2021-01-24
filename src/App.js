a1b9ff9c7609b213bbd2633b7dafd01c599bbbd3
Christian Possidonio <christian@orientme.com.br>
2021-01-24 19:54:19 -0300

    Dark mode remove

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { createMuiTheme, ThemeProvider, useMediaQuery, CssBaseline } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider, CssBaseline } from '@material-ui/core';
 import Home from './pages/Home';
 
 function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)');
-
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          type: prefersDarkMode ? 'dark' : 'light',
-          primary: {
-            main: '#fbc531',
-          },
-          secondary: {
-            main: '#e4e450',
-          },
+  const theme = React.useMemo(() =>
+    createMuiTheme({
+      palette: {
+        primary: {
+          main: '#fbc531',
         },
-      }),
-    [prefersDarkMode]
+        secondary: {
+          main: '#e4e450',
+        },
+      },
+    })
   );
 
   return (
